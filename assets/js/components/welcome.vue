10aafc2241038573c86615c1c58b6b16d81ab677
farhan <farhan711>
2017-10-26 20:53:30 +0530

    WordVuePress inital commit

---
+++ assets/js/components/welcome.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Welcome to WordVuePress</h1>
+</template>
+  
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+
